711d4ec88c3e3b5244faf10613d21fb56804de38
xuyazhou <blake.xu@transsnet.com>
2021-09-10 18:32:39 +0800

    修改日志显示

--- app/src/main/java/com/transsnet/transsdktest/base/BaseApplication.java
+++ app/src/main/java/com/transsnet/transsdktest/base/BaseApplication.java
@@ -3,6 +3,7 @@ package com.transsnet.transsdktest.base;
 import androidx.multidex.MultiDexApplication;
 
 import com.transsnet.transsdk.manager.TransConfigManager;
+import com.transsnet.transsdktest.utils.Logger;
 
 public class BaseApplication extends MultiDexApplication {
 
@@ -12,5 +13,6 @@ public class BaseApplication extends MultiDexApplication {
         // 此处必须配置channelKey否则无法使用
         TransConfigManager.getTransConfig().init(this, "input your ChannelKey");
         TransConfigManager.getTransConfig().setLoginEnabled(true);
+        Logger.isDebug = true;
     }
 }
